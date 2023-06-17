2f8c19afcc6d6fb6d2d5a5d11913dbf4205e6f97
Vinicius <viniciusjpnworkspaceg@gmail.com>
2023-06-17 18:29:01 -0300

    add rcy and scroll

---
+++ app/src/main/java/com/example/despesasemdia_dd/paginainicial/PaginaInicial.kt
@@ -0,0 +1,12 @@
+package com.example.despesasemdia_dd.paginainicial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.despesasemdia_dd.R
+
+class PaginaInicial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagina_inicial)
+    }
+}
