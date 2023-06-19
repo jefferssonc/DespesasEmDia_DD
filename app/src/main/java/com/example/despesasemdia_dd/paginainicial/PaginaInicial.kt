2289a8bf86e043bdc98b6cf3699e15229425c34b
Vinicius <viniciusjpnworkspaceg@gmail.com>
2023-06-19 15:21:22 -0300

    adapter + recy add

--- app/src/main/java/com/example/despesasemdia_dd/paginainicial/PaginaInicial.kt
+++ app/src/main/java/com/example/despesasemdia_dd/paginainicial/PaginaInicial.kt
@@ -2,11 +2,30 @@ package com.example.despesasemdia_dd.paginainicial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.despesasemdia_dd.R
+import com.example.despesasemdia_dd.adapter.AdapterDespesa
+import com.example.despesasemdia_dd.model.Despesa
 
 class PaginaInicial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagina_inicial)
+
+        val recyclerView_despesas = findViewById<RecyclerView>(R.id.recyclerView_despesas)
+        recyclerView_despesas.layoutManager= LinearLayoutManager(this)
+        recyclerView_despesas.setHasFixedSize(true)
+        //Configuração do adapter
+        val listaDespesas: MutableList<Despesa> = mutableListOf()
+        val adapterDespesa = AdapterDespesa(this,listaDespesas)
+        recyclerView_despesas.adapter = adapterDespesa
+        //criando items
+        val despesa1 = Despesa(
+            R.drawable.home_house_icon,
+            nome = "Residencia",
+            preco = "R$ 899,34"
+        )
+        listaDespesas.add(despesa1)
     }
 }
