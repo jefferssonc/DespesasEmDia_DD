613dc8a48de6dfcd3289eb17562b6e4266218759
jefferssonc <92582232+jefferssonc@users.noreply.github.com>
2023-06-30 20:06:49 -0300

    Layout Grupo

---
+++ app/src/main/java/com/example/despesasemdia_dd/grupo/Grupo.kt
@@ -0,0 +1,12 @@
+package com.example.despesasemdia_dd.grupo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.despesasemdia_dd.R
+
+class Grupo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grupo)
+    }
+}
