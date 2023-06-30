c7c8c7d8fdd742e32d3e9c104e6278c6c511d863
jefferssonc <92582232+jefferssonc@users.noreply.github.com>
2023-06-30 18:56:02 -0300

    Layout Grupos

---
+++ app/src/main/java/com/example/despesasemdia_dd/grupos/Grupos.kt
@@ -0,0 +1,12 @@
+package com.example.despesasemdia_dd.grupos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.despesasemdia_dd.R
+
+class Grupos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grupos)
+    }
+}
