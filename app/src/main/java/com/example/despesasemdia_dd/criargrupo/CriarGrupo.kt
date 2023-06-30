245d28061f197af2588eabff3f556e9569393738
jefferssonc <92582232+jefferssonc@users.noreply.github.com>
2023-06-30 20:23:56 -0300

    Criar Grupo

---
+++ app/src/main/java/com/example/despesasemdia_dd/criargrupo/CriarGrupo.kt
@@ -0,0 +1,12 @@
+package com.example.despesasemdia_dd.criargrupo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.despesasemdia_dd.R
+
+class CriarGrupo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criar_grupo)
+    }
+}
