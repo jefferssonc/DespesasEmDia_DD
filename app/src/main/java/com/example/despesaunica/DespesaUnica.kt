6e90c2388c1cee19f3f16ef50e728d26bf1102e3
Vinicius <viniciusjpnworkspaceg@gmail.com>
2023-07-02 23:58:49 -0300

    listas adicionadas porem falta os adapters

---
+++ app/src/main/java/com/example/despesaunica/DespesaUnica.kt
@@ -0,0 +1,12 @@
+package com.example.despesaunica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.despesasemdia_dd.R
+
+class DespesaUnica : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_despesa_unica)
+    }
+}
