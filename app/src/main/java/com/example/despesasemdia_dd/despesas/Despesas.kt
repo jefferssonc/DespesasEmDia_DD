3d46138becd0a2b8880fa716394447cd7d0116e8
jefferssonc <92582232+jefferssonc@users.noreply.github.com>
2023-07-04 23:40:57 -0300

    Layouts que faltavam criados. Falta recyclewviews,adapters, etc

--- app/src/main/java/com/example/despesasemdia_dd/despesas/Despesas.kt
+++ app/src/main/java/com/example/despesasemdia_dd/despesas/Despesas.kt
@@ -1,12 +1,12 @@
-package com.example.despesasemdia_dd.grupo
+package com.example.despesasemdia_dd.despesas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.despesasemdia_dd.R
 
-class Grupo : AppCompatActivity() {
+class Despesas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_grupo)
+        setContentView(R.layout.activity_despesas)
     }
 }
--- app/src/main/java/com/example/despesasemdia_dd/despesaunica/DespesaUnica.kt
+++ app/src/main/java/com/example/despesasemdia_dd/despesaunica/DespesaUnica.kt
@@ -1,12 +1,12 @@
-package com.example.despesasemdia_dd.grupos
+package com.example.despesasemdia_dd.despesaunica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.despesasemdia_dd.R
 
-class Grupos : AppCompatActivity() {
+class DespesaUnica : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_grupos)
+        setContentView(R.layout.activity_despesa_unica)
     }
 }
